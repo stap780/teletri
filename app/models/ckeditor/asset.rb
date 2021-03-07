d23284c9909af9b21c75bb0a3517db3b03ccc8a0
stap780 <panaet80@mail.ru>
2021-03-07 19:13:04 +0300

    change gemfile

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -2,5 +2,9 @@
 
 class Ckeditor::Asset < ActiveRecord::Base
   include Ckeditor::Orm::ActiveRecord::AssetBase
-  include Ckeditor::Backend::Paperclip
+  include Ckeditor::Backend::ActiveStorage
+
+  attr_accessor :data
+
+  has_one_attached :storage_data
 end
