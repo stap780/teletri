537c5063f04d98101cc278c24aec9d8aa2b4c7fa
stap780 <panaet80@mail.ru>
2021-03-08 11:04:04 +0300

    spree_editor

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Ckeditor::Asset < ActiveRecord::Base
-  include ActiveRecord
   include Ckeditor::Orm::ActiveRecord::AssetBase
   include Ckeditor::Backend::ActiveStorage
 
